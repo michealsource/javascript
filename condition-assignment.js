9b4f554e440c1929db1ebd2f90ab16e552865190
michealsource <kelvinmicheal209@gmail.com>
2022-12-23 09:27:17 +0100

    new commit

---
+++ condition-assignment.js
@@ -0,0 +1,32 @@
+/*
+    =========EXCERISE NUMBER ONE=========
+    1. Create a variable called points that starts at 40
+    2. Create an if statement that checks if points are greater than 20
+    3. If points are greater than 20, subtract 10 from points
+    4. Log out the points at the end of the program (this should always log)
+    5. Change points to start at 15 and run the program again
+*/
+
+/*
+
+  =========EXCERISE NUMBER TWO=========
+    1. At the start of the program print out: "--- Animal Checker Program ---"
+    2. Create a variable called animal that starts at the value "cat"
+    3. Create an if statement that checks if animal is equal to "cat"
+    4. If it is equal to "cat", print out "Meow!" to the screen
+    5. If it is equal to "dog", print out "Woof!" to the screen
+    6. If it is neither "cat" nor "dog", print out "Must be an alien"
+    7. At the end of the program print out: "--- Animal check complete ---"
+    8. Change animal to different values to make sure it works in all cases
+*/
+
+/*
+ =========EXCERISE NUMBER THREE=========
+    Create a program that starts with a number variable.
+    The program will print out whether the variable is
+    even or odd to the screen when run.
+
+    Change the variable to test it works in both cases.
+
+    HINT: modulus operator ( % ) is your friend
+*/
---
+++ script.js
@@ -0,0 +1,64 @@
+// ARITHEMETIC OPERATORS
+//   let num1 = 5;
+//   let num2 = 18;
+// console.log(num2 + num1)
+// console.log(num2 - num1)
+// console.log(num2 * num1)
+// console.log(num2 / num1)
+// console.log(num2 % num1)
+
+// RELATIONAL OPERATORS
+// > < >= <= != ==
+// console.log(num1 == num2)
+
+// LOGICAL OPERATORS
+// && ||
+//   console.log()
+
+// ASSIGNMENT OPERATORS
+// = += -= *= /= %=
+
+// console.log(num1 = num1 + 20)
+
+// UNARY OR DECREMENT OR INCREMENT OPERATORS
+// ++ --
+//   num1--;
+//   console.log(num1)
+
+// let name = "promise wed 3innocent"
+// let age = 90
+// let ismale = true
+// let money = null
+// let course = undefined
+
+
+
+// =======CONTROL FLOW ========//
+
+// let points = 60
+// let age = 15
+
+//  flase         true
+// if (age >= 20 || age <= 50) {
+//    console.log('hello its bigger')
+// } else {
+//    console.log('hello its not bigger')
+// }
+
+// console.log("always showing")
+
+
+let grade = 3
+
+if (grade >= 70 && grade <= 100) {
+   console.log("A")
+}
+else if (grade >= 60 && grade < 70) {
+   console.log('B')
+}
+else if (grade >= 45 && grade < 60) {
+   console.log("C")
+}
+else {
+   console.log('F9')
+}
